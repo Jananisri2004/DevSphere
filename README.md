5b4768dfb268be6e744322a2076f9268ccaab674
Jananisri2004 <jananisriv2004@gmail.com>
2026-02-15 16:03:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# DevSphere
+
+A full-stack profile management application built with:
+
+- React (Vite)
+- Express.js
+- MongoDB
+- Mongoose
+- bcrypt authentication
+
+## Features
+- User Registration
+- Login Authentication
+- Profile Dashboard
+- Edit Profile (MongoDB update)
+- Protected Routes
+
+## Run Locally
+
+### Backend
+cd backend
+npm install
+node server.js
+
+### Frontend
+cd frontend
+npm install
+npm run dev
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+const handleLogin = async () => {
+  try {
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      localStorage.setItem("user", JSON.stringify(data.user));
+      alert("Login Successful ✅");
+      window.location.href = "/profile";
+    } else {
+      alert(data.message);
+    }
+  } catch (error) {
+    alert("Server error");
+  }
+};
+  return (
+    <div className="container">
+      <h2>Login</h2>
+
+      <input
+        type="text"
+        placeholder="Email or Username"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
