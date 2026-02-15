3eb0867f913628c0608df1a48d9eeaac269e84c8
Jananisri2004 <jananisriv2004@gmail.com>
2026-02-15 17:54:12 +0530

    fix

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
   const [password, setPassword] = useState("");
 const handleLogin = async () => {
   try {
-    const response = await fetch("http://localhost:5000/login", {
+    const response = await fetch("https://devsphere-backend-r83a.onrender.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
